cffb6346fd9498b7c233bbf8fbf5dc39a416a7ce
madgrv <mad.grv@gmail.com>
2025-05-01 17:42:11 +0200

    fixed API Key type and added .env file with keys for deployment

--- new-weather-app/src/components/AppLayout.tsx
+++ new-weather-app/src/components/AppLayout.tsx
@@ -1,61 +1,64 @@
 import React, { useState } from 'react';
-import { HorizontalWrapper, VerticalWrapper} from './_common';
-import { Title, Paragraph, BoldText } from './_common/_typography'
-import {DateDisplay} from './DateDisplay';
-import {SearchInput} from './SearchInput';
-import {WeatherDisplay} from './WeatherDisplay';
-import { Location } from '../types'
-import { APIKEY } from '../helpers';
+import { HorizontalWrapper, VerticalWrapper } from './_common';
+import { Title, Paragraph, BoldText } from './_common/_typography';
+import { DateDisplay } from './DateDisplay';
+import { SearchInput } from './SearchInput';
+import { WeatherDisplay } from './WeatherDisplay';
+import { Location } from '../types';
+import { APIKEY } from '../helpers'; 
 import { getLocation } from '../api/getLocation';
 
 export const AppLayout = () => {
-	const [userInput, setUserInput] = useState('');
-	const [locations, setLocations] = useState([]);
-	const [selectedLocation, setSelectedLocation] = useState<Location | null>(null);
+  const [userInput, setUserInput] = useState('');
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState<Location | null>(
+    null
+  );
 
-	const handleSearch = async () => {
-		setLocations(await getLocation(userInput, APIKEY));
-	};
+  const handleSearch = async () => {
+    if (!userInput || !APIKEY) return; 
+    setLocations(await getLocation(userInput, APIKEY));
+  };
 
-	const handleLocationSelect = (location: Location) => {
-		setSelectedLocation(location);
-		setLocations([]);
-	};
+  const handleLocationSelect = (location: Location) => {
+    setSelectedLocation(location);
+    setLocations([]);
+  };
 
-	return (
-		<>
-			<VerticalWrapper>
-				<HorizontalWrapper $justify={'space-between'}>
-					<Title>WeatherUp!</Title>
-					<Paragraph>World Weather</Paragraph>
-					<DateDisplay />
-				</HorizontalWrapper>
-			</VerticalWrapper>
-			<VerticalWrapper>
-				<SearchInput
-					locations={locations}
-					userInput={userInput}
-					setUserInput={setUserInput}
-					handleSearch={handleSearch}
-					handleLocationSelect={handleLocationSelect}
-				/>
-				{selectedLocation ? (
-					<WeatherDisplay selectedLocation={selectedLocation} APIKEY={APIKEY} />
-				) : (
-					<>
-						<VerticalWrapper>
-							<Paragraph>
-								<BoldText>Welcome to WeatherUp! 🌤️</BoldText>
-							</Paragraph>
-						</VerticalWrapper>
-						<Paragraph>
-							Enter a city name to check the weather and stay informed about
-							current conditions. Get started by typing in the search box above.
-							Happy exploring!
-						</Paragraph>
-					</>
-				)}
-			</VerticalWrapper>
-		</>
-	);
+  return (
+    <>
+      <VerticalWrapper>
+        <HorizontalWrapper $justify={'space-between'}>
+          <Title>WeatherUp!</Title>
+          <Paragraph>World Weather</Paragraph>
+          <DateDisplay />
+        </HorizontalWrapper>
+      </VerticalWrapper>
+      <VerticalWrapper>
+        <SearchInput
+          locations={locations}
+          userInput={userInput}
+          setUserInput={setUserInput}
+          handleSearch={handleSearch}
+          handleLocationSelect={handleLocationSelect}
+        />
+        {selectedLocation ? (
+          <WeatherDisplay selectedLocation={selectedLocation} APIKEY={APIKEY || ''} />
+        ) : (
+          <>
+            <VerticalWrapper>
+              <Paragraph>
+                <BoldText>Welcome to WeatherUp! 🌤️</BoldText>
+              </Paragraph>
+            </VerticalWrapper>
+            <Paragraph>
+              Enter a city name to check the weather and stay informed about
+              current conditions. Get started by typing in the search box above.
+              Happy exploring!
+            </Paragraph>
+          </>
+        )}
+      </VerticalWrapper>
+    </>
+  );
 };
--- new-weather-app/src/helpers.js
+++ new-weather-app/src/helpers.js
@@ -1,55 +1,55 @@
 // helpers.js
 // Dummy comment to force rebuild - version 2
 
-export const APIKEY = '6f8ae8c028d2cc301f42425357121375';
+export const APIKEY = process.env.APP_API_KEY;
 
 // function to create a flag emoji from ASCII
 export function getFlagEmoji(countryCode) {
-	const countryCodeUpperCase = countryCode.toUpperCase();
-	const OFFSET = 127397;
-	return countryCodeUpperCase
-		.split('')
-		.map((char) => String.fromCodePoint(char.charCodeAt(0) + OFFSET))
-		.join('');
+  const countryCodeUpperCase = countryCode.toUpperCase();
+  const OFFSET = 127397;
+  return countryCodeUpperCase
+    .split('')
+    .map((char) => String.fromCodePoint(char.charCodeAt(0) + OFFSET))
+    .join('');
 }
 
 // helper function to convert unix timestamp into time format (hh:mm)
 export function convertTimestamp(timestamp) {
-	return new Date(timestamp * 1000).toLocaleTimeString([], {
-		hour: '2-digit',
-		minute: '2-digit',
-		// hour12: false,
-	});
+  return new Date(timestamp * 1000).toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    // hour12: false,
+  });
 }
 
 // Function to convert timezone data to UTC readable values
 export function getTimezoneName(timezoneOffset) {
-	// Adjust the timezone name based on the offset
-	const hours = Math.floor(Math.abs(timezoneOffset) / 3600);
-	const minutes = Math.abs(timezoneOffset % 3600) / 60;
-	const sign = timezoneOffset >= 0 ? '+' : '-';
-	const offsetString = `${sign}${hours.toString().padStart(2, '0')}:${minutes
-		.toString()
-		.padStart(2, '0')}`;
+  // Adjust the timezone name based on the offset
+  const hours = Math.floor(Math.abs(timezoneOffset) / 3600);
+  const minutes = Math.abs(timezoneOffset % 3600) / 60;
+  const sign = timezoneOffset >= 0 ? '+' : '-';
+  const offsetString = `${sign}${hours.toString().padStart(2, '0')}:${minutes
+    .toString()
+    .padStart(2, '0')}`;
 
-	// return `UTC${offsetString}`;
-	return offsetString;
+  // return `UTC${offsetString}`;
+  return offsetString;
 }
 
 // Function to get weather icon based on weather description
 export function getWeatherIcon(weatherDescription) {
-	const weatherIcons = {
-		Clear: '☀️',
-		Clouds: '☁️',
-		Rain: '🌧️',
-		Thunderstorm: '⛈️',
-		Drizzle: '🌦️',
-		Snow: '❄️',
-	};
+  const weatherIcons = {
+    Clear: '☀️',
+    Clouds: '☁️',
+    Rain: '🌧️',
+    Thunderstorm: '⛈️',
+    Drizzle: '🌦️',
+    Snow: '❄️',
+  };
 
-	const condition = Object.keys(weatherIcons).find((key) =>
-		weatherDescription.toLowerCase().includes(key.toLowerCase())
-	);
+  const condition = Object.keys(weatherIcons).find((key) =>
+    weatherDescription.toLowerCase().includes(key.toLowerCase())
+  );
 
-	return weatherIcons[condition] || '';
+  return weatherIcons[condition] || '';
 }
